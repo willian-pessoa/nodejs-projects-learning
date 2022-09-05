0a0678a10f7624c84f46cd4d969a2753cc572105
willian-pessoa <willianpessoadeabreu@gmail.com>
2022-09-05 20:58:39 -0300

    restful apis, crud, sending files, serving website with node, template engines

--- express-project/controllers/messages.controller.js
+++ express-project/controllers/messages.controller.js
@@ -1,5 +1,13 @@
+const path = require("path")
+
 function getMessages(req, res) {
-	res.send("<h1>Helloo Willian !!!</h1>")
+	res.render("messages", {
+		title: "Message to my friend",
+		friend: "Amelia Duckworth"
+	})
+	//res.sendFile(path.join(__dirname, "..", "public", "images", "wow.jpg"))
+	//res.sendFile(path.join(__dirname, "..", "public", "index.html"))
+	//res.send("<h1>Helloo Willian !!!</h1>")
 }
 
 function postMessages(req, res) {

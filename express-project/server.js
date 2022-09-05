c5a82b7c3985c0c950b0f384c713acfe91af452b
willian-pessoa <willianpessoadeabreu@gmail.com>
2022-09-05 14:55:22 -0300

    add middleware

--- express-project/server.js
+++ express-project/server.js
@@ -5,34 +5,41 @@ const app = express()
 const PORT = 3000
 
 const friends = [
-    {
-        id: 0,
-        name: "Albert Einstein"
-    },
-    {
-        id: 1,
-        name: "Willian Pessoa"
-    }
+	{
+		id: 0,
+		name: "Albert Einstein"
+	},
+	{
+		id: 1,
+		name: "Willian Pessoa"
+	}
 ]
 
+app.use((req, res, next) => {
+	const start = Date.now()
+	next();
+	const delta = Date.now() - start
+	console.log(`${req.method} ${req.url} ${delta}ms`)
+})
+
 app.get("/friends", (req, res) => {
-    res.json(friends)
+	res.json(friends)
 })
 
 app.get("/friends/:friendsId", (req, res) => {
-    const friendId = Number(req.params.friendsId);
-    const friend = friends[friendId];
+	const friendId = Number(req.params.friendsId);
+	const friend = friends[friendId];
 
-    if (friend){
-        res.json(friend)
-    } else {
-        res.status(404).json({
-            error: "Friend doesn't found"
-        })
-    }
+	if (friend) {
+		res.json(friend)
+	} else {
+		res.status(404).json({
+			error: "Friend doesn't found"
+		})
+	}
 })
 
 app.listen(PORT, () => {
-    console.log(`Listening on ${PORT}...`)
+	console.log(`Listening on ${PORT}...`)
 })
 
